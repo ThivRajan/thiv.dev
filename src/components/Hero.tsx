2ef3287bc39ec567306cf515740c9e9a745a7acb
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2024-02-25 16:52:18 -0500

    Fix responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,19 +5,19 @@ import avatar from '../assets/Memoji Avatar.png'
 
 const ROLES = [
 	'Full Stack Software Developer',
-	'Fantasy/sci-fi Junkie',
+	'Fantasy/Sci-fi Junkie',
 	'Plotting world domination',
 ].map((role) => `// ${role}`)
 
 export default function Hero() {
 	return (
-		<div className="flex gap-8">
-			<img src={avatar.src} alt="avatar" className=" w-1/3" />
-			<div className="flex flex-col gap-4 justify-center">
+		<div className="flex flex-col items-center sm:flex-row gap-8">
+			<img src={avatar.src} alt="avatar" className="w-1/3" />
+			<div className="flex flex-col gap-4 w-full">
 				<h1 className="drop-shadow-glow text-5xl font-medium bg-gradient-to-r from-neutral-200  to-red-800 text-transparent bg-clip-text max-w-fit">
 					Thiv Rajan
 				</h1>
-				<div className="bg-slate-800 rounded p-4 w-[35ch]">
+				<div className="bg-slate-800 rounded p-4 min-w-[35ch] max-w-full sm:w-[35ch]">
 					<code className="text-gray-400 italic">
 						<Typewriter
 							options={
