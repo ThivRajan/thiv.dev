4c83b12a85d45b9c166ad3a42d6729a51153878f
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2024-02-26 21:36:56 -0500

    Add stub About section

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+	return (
+		<div className="text-white flex flex-col gap-2">
+			<h1 className="font-medium text-3xl">About</h1>
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting
+				industry. Lorem Ipsum has been the industry standard dummy text ever
+				since the 1500s, when an unknown printer took a galley of type and
+				scrambled it to make a type specimen book.
+			</p>
+		</div>
+	)
+}
