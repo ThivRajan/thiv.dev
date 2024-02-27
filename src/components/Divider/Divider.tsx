b31f62809780656712a48e29ae79a92b8a58b2ab
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2024-02-26 21:48:01 -0500

    Update Divider styles

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,9 +1,19 @@
 import './Divider.css'
 
+const ANIMATION_STYLES = 'transition-all duration-500'
+
 export default function Divider() {
 	return (
-		<div className="flex justify-center">
-			<div id="infinity" />
+		<div className="divider flex justify-center items-center gap-3">
+			<StyledRuler />
+			<div
+				className={`square bg-slate-500 transition-all ${ANIMATION_STYLES}`}
+			/>
+			<StyledRuler />
 		</div>
 	)
 }
+
+const StyledRuler = () => (
+	<hr className={`border-1px border-slate-500 w-1/5 ${ANIMATION_STYLES}`} />
+)
