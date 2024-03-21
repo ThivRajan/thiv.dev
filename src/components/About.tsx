83535be55157ef19f9cd03e6e790e82ad0d0ef5b
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2024-03-21 16:58:59 -0400

    Add text and styles to About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,21 @@
+import './About.css'
+
 export default function About() {
 	return (
 		<div className="text-white flex flex-col gap-2">
 			<h1 className="font-medium text-3xl">About</h1>
-			<p>
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book.
+			<p className="about-text">
+				Hi there! Welcome to my portfolio. I&apos;m currently working at{' '}
+				<a
+					href="https://www.canada.ca/en/environment-climate-change.html"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Environment & Climate Change Canada
+				</a>
+				. I love to get lost in a problem almost as much as finding my way out
+				of one. Outside of work you&apos;ll find me engrossed in a book or a
+				show.
 			</p>
 		</div>
 	)
