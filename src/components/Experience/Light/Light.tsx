02e6895a2ccd4f97b253183278c2ff7d7e8cdc02
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2024-03-27 10:34:38 -0400

    Move Light colour behaviour to CSS file

--- src/components/Experience/Light/Light.tsx
+++ src/components/Experience/Light/Light.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { type CSSProperties } from 'react'
 import './Light.css'
 
 export default function Light({
@@ -10,30 +10,19 @@ export default function Light({
 	setSelected: any
 	color: string
 }) {
-	const [isHovered, setIsHovered] = useState(false)
-
 	return (
 		<div
-			onMouseOver={() => setIsHovered(true)}
-			onMouseOut={() => setIsHovered(false)}
-			className="square-wrapper cursor-pointer"
-			style={{
-				filter:
-					selected || isHovered ? `drop-shadow(0 -4px 10px ${color})` : '',
-			}}
+			className={`square-wrapper ${selected && 'selected'} cursor-pointer`}
+			style={
+				{
+					'--light-bg': color,
+				} as CSSProperties
+			}
 			onClick={setSelected}
 		>
 			<div className="square bg-slate-800">
-				<div
-					className="light-wrapper"
-					style={{
-						filter: selected ? `drop-shadow(0 0 3px ${color})` : '',
-					}}
-				>
-					<div
-						className="light shadow-md"
-						style={{ background: selected ? color : 'rgb(30 41 59)' }}
-					></div>
+				<div className={`light-wrapper ${selected && 'selected'}`}>
+					<div className="light shadow-md" />
 				</div>
 			</div>
 		</div>
