516c252ccd0fcfec8efc552b6ee7d6fc5283256c
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2024-04-02 14:58:01 -0400

    Change contact colours

--- src/components/ContactBar/contact.config.ts
+++ src/components/ContactBar/contact.config.ts
@@ -2,12 +2,12 @@ export const CONTACT_CONFIG = [
 	{
 		icon: 'basil:linkedin-solid',
 		link: 'https://www.linkedin.com/in/thiv-rajan',
-		color: '#314e7d',
+		color: '#8db1f0',
 	},
 	{
 		icon: 'codicon:github',
 		link: 'https://github.com/ThivRajan',
-		color: '#525252',
+		color: '#a8a8a8',
 	},
-	{ icon: 'bxl:gmail', link: 'mailto:thiv.rajan@gmail.com', color: '#bf6262' },
+	{ icon: 'bxl:gmail', link: 'mailto:thiv.rajan@gmail.com', color: '#e0a2a2' },
 ]
