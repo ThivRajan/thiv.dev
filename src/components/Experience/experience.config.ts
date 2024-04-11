1228579cd0fd0fbf7e2c6cd9c242ad07e11d30b0
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2024-04-10 20:41:24 -0400

    Update Experience to use bullet points

--- src/components/Experience/experience.config.ts
+++ src/components/Experience/experience.config.ts
@@ -1,20 +1,35 @@
 export const EXPERIENCE_CONFIG = [
 	{
 		title: 'Environment & Climate Change Canada',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+		jobPoints: [
+			'Led a cross-functional team in an Agile Environment to deliver a weather station editor',
+			'Developed reusable dashboards and grids to allow for remote sensor viewing',
+			'Mentored interns on Angular architecture, version control, and writing documentation, fostering both technical and communication skills',
+		],
 		color: 'red',
 	},
 	{
 		title: 'Cion Studio',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-		color: '#1fa4e0',
+		jobPoints: [
+			'Transformed the user experience of a skate-sharpening e-commerce store by re-building login, ordering, and checkout pages, allowing users to quickly place finely-tuned sharpening orders',
+			'Crafted custom static sites for small businesses across various platforms and strategically optimized SEO to boost online reach and expand their client base',
+		],
+		color: 'green',
 	},
 	{
 		title: 'IT Collaborative Inc.',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-		color: 'green',
+		jobPoints: [
+			'Built login, home, and settings pages for both the cross-platform and iOS implementations of a mobile app that allows users to securely access their private documents',
+			"Engineered server-less functions to support new features and  metrics integrations of the company's iOS and Android applications",
+		],
+		color: 'blue',
+	},
+	{
+		title: 'Canadian Institutes of Health Research',
+		jobPoints: [
+			"Streamlined the workflow of the organization's CRM tool by building custom plugins",
+			'Accelerated legacy CRM retirement timelines by completing QA testing on CRM prototype weeks ahead of schedule',
+		],
+		color: 'white',
 	},
 ]
