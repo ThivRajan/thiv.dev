1bc32cadcdcd21bda855801d1a792d64d8a6de4d
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2025-09-16 20:35:07 -0400

    Remove conflicting styles in ContactButton

--- src/components/ContactsBar/ContactButton.tsx
+++ src/components/ContactsBar/ContactButton.tsx
@@ -16,7 +16,7 @@ export default function ContactButton({
 			target="_blank"
 			rel="noreferrer"
 			style={{ '--hover-color': hoverColor } as React.CSSProperties}
-			className={`block p-2 w-fit text-2xl border-2 border-gray-400 text-gray-400 hover:border-[var(--hover-color)] hover:text-[var(--hover-color)] hover:border-white hover:text-white rounded cursor-pointer shadow-md transition-all duration-700`}
+			className={`block p-2 w-fit text-2xl border-2 border-gray-400 text-gray-400 hover:border-[var(--hover-color)] hover:text-[var(--hover-color)] rounded cursor-pointer shadow-md transition-all duration-700`}
 		>
 			<Icon icon={icon} />
 		</a>
