808ef2b0f4d06597ab26daaf94631274af192865
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2025-09-20 20:06:08 -0400

    Clean up Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import avatar from '@assets/favicon.png'
+import favicon from '@assets/favicon.png'
 import Typewriter, { type Options } from 'typewriter-effect'
 import ContactsBar from './ContactsBar/ContactsBar'
 
@@ -7,7 +7,7 @@ const ROLES = ['Fullstack Software Engineer'].map((role) => `// ${role}`)
 export default function Hero() {
 	return (
 		<div className="flex flex-col items-center sm:flex-row justify-between gap-6">
-			<img src={avatar.src} alt="avatar" className="w-1/3 float-wrapper" />
+			<img src={favicon.src} alt="favicon" className="w-1/3 float-wrapper" />
 			<div className="flex flex-col gap-4">
 				<div className="flex flex-col items-center sm:items-start gap-2 w-full">
 					<h1 className="text-5xl font-bold">Thiv Rajan</h1>
